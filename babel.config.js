852213d6824463a64946fba735927771326a4202
tazirahmb <tazirahmb@outlook.com>
2021-02-28 21:03:30 +0700

    fix module resolver bug

--- babel.config.js
+++ babel.config.js
@@ -2,19 +2,16 @@ module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     [
-      'babel-plugin-root-import',
-      { rootPathPrefix: '~', rootPathSuffix: 'src' },
-    ],
-    [
       'module-resolver',
       {
-        root: ['./src'],
+        root: ['.'],
         extensions: ['.ios.js', '.android.js', '.js', '.json'],
         alias: {
-          '@elements': './components/elements',
-          '@forms': './components/forms',
-          '@fragments': './components/fragments',
-          '@layouts': './components/layouts',
+          '~': './src',
+          '@elements': './src/components/elements',
+          '@forms': './src/components/forms',
+          '@fragments': './src/components/fragments',
+          '@layouts': './src/components/layouts',
         },
       },
     ],
