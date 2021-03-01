112408920e8860ab11050d73f5dac581a99376c6
tazirahmb <tazirahmb@outlook.com>
2021-03-01 15:24:56 +0700

    finish initialize husky

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 // const Tab = createBottomTabNavigator();
 
-// const Stack = createStackNavigator();
+const Stack = createStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
